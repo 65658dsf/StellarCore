5fae576ca9b2fec4a35983401540823bad71d2cf
NingMeng123 <2157769082@qq.com>
2025-11-15 21:22:43 +0800

    fix(build): 修正构建脚本中输出目录清理和编译目标路径
    修改清理output目录时明确指定StellarCore子目录
    将所有编译目标从frpc改为frps并更新压缩包命名格式

--- build_frps.bat
+++ build_frps.bat
@@ -7,10 +7,11 @@ setlocal EnableDelayedExpansion
 set VERSION=1.1.0
 set BINARY_NAME=StellarCore
 
+
 :: 清空输出目录
 if exist output (
-    echo 正在清空 output 目录...
-    rmdir /s /q output
+    echo 正在清空 output\StellarCore 目录...
+    rmdir /s /q output\StellarCore
 )
 mkdir output
 mkdir output\StellarCore
@@ -35,126 +36,126 @@ if %ERRORLEVEL% NEQ 0 (
 echo 正在编译 Windows 版本...
 set GOOS=windows
 set GOARCH=386
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%.exe"
-powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%VERSION%_windows_386_%GIT_VERSION%.zip' -Force"
+powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%BINARY_NAME%_%VERSION%_windows_386.zip' -Force"
 del "output\StellarCore\%BINARY_NAME%.exe"
 
 set GOOS=windows
 set GOARCH=amd64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%.exe"
-powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%VERSION%_windows_amd64_%GIT_VERSION%.zip' -Force"
+powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%BINARY_NAME%_%VERSION%_windows_amd64.zip' -Force"
 del "output\StellarCore\%BINARY_NAME%.exe"
 
 set GOOS=windows
 set GOARCH=arm
 set GOARM=7
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%.exe"
-powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%VERSION%_windows_arm32_%GIT_VERSION%.zip' -Force"
+powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%BINARY_NAME%_%VERSION%_windows_arm32.zip' -Force"
 del "output\StellarCore\%BINARY_NAME%.exe"
 
 set GOOS=windows
 set GOARCH=arm64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%.exe" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%.exe"
-powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%VERSION%_windows_arm64_%GIT_VERSION%.zip' -Force"
+powershell -Command "Compress-Archive -Path 'output/StellarCore/%BINARY_NAME%.exe' -DestinationPath 'output/StellarCore/%BINARY_NAME%_%VERSION%_windows_arm64.zip' -Force"
 del "output\StellarCore\%BINARY_NAME%.exe"
 
 :: Linux
 echo 正在编译 Linux 版本...
 set GOOS=linux
 set GOARCH=386
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_linux_386_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_linux_386_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_linux_386_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_linux_386_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_linux_386.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_linux_386.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_linux_386.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_linux_386.tar"
 
 set GOOS=linux
 set GOARCH=amd64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_linux_amd64_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_linux_amd64_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_linux_amd64_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_linux_amd64_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_linux_amd64.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_linux_amd64.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_linux_amd64.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_linux_amd64.tar"
 
 set GOOS=linux
 set GOARCH=arm
 set GOARM=5
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_linux_arm32v5_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_linux_arm32v5_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_linux_arm32v5_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_linux_arm32v5_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v5.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v5.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v5.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_linux_arm32v5.tar"
 
 set GOOS=linux
 set GOARCH=arm
 set GOARM=6
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_linux_arm32v6_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_linux_arm32v6_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_linux_arm32v6_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_linux_arm32v6_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v6.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v6.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v6.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_linux_arm32v6.tar"
 
 set GOOS=linux
 set GOARCH=arm
 set GOARM=7
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_linux_arm32v7_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_linux_arm32v7_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_linux_arm32v7_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_linux_arm32v7_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v7.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v7.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm32v7.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_linux_arm32v7.tar"
 
 set GOOS=linux
 set GOARCH=arm64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_linux_arm64_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_linux_arm64_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_linux_arm64_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_linux_arm64_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm64.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm64.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_linux_arm64.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_linux_arm64.tar"
 
 :: FreeBSD
 echo 正在编译 FreeBSD 版本...
 set GOOS=freebsd
 set GOARCH=386
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_freebsd_386_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_freebsd_386_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_freebsd_386_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_freebsd_386_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_386.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_386.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_386.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_freebsd_386.tar"
 
 set GOOS=freebsd
 set GOARCH=amd64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_freebsd_amd64_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_freebsd_amd64_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_freebsd_amd64_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_freebsd_amd64_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_amd64.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_amd64.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_amd64.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_freebsd_amd64.tar"
 
 set GOOS=freebsd
 set GOARCH=arm
 set GOARM=7
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_freebsd_arm32_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_freebsd_arm32_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_freebsd_arm32_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_freebsd_arm32_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_arm32.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_arm32.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_arm32.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_freebsd_arm32.tar"
 
 set GOOS=freebsd
 set GOARCH=arm64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_freebsd_arm64_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_freebsd_arm64_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_freebsd_arm64_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_freebsd_arm64_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_arm64.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_arm64.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_freebsd_arm64.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_freebsd_arm64.tar"
 
 :: macOS
 echo 正在编译 macOS 版本...
 set GOOS=darwin
 set GOARCH=amd64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_darwin_amd64_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_darwin_amd64_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_darwin_amd64_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_darwin_amd64_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_darwin_amd64.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_darwin_amd64.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_darwin_amd64.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_darwin_amd64.tar"
 
 set GOOS=darwin
 set GOARCH=arm64
-go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frpc
+go build -trimpath -ldflags "-s -w" -o "output/StellarCore/%BINARY_NAME%" ./cmd/frps
 upx --best --lzma "output/StellarCore/%BINARY_NAME%"
-cd output && 7z a -ttar "StellarCore/%VERSION%_darwin_arm64_%GIT_VERSION%.tar" "StellarCore/%BINARY_NAME%" && 7z a -tgzip "StellarCore/%VERSION%_darwin_arm64_%GIT_VERSION%.tar.gz" "StellarCore/%VERSION%_darwin_arm64_%GIT_VERSION%.tar" && cd ..
-del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%VERSION%_darwin_arm64_%GIT_VERSION%.tar"
+cd output/StellarCore && 7z a -ttar "../StellarCore/%BINARY_NAME%_%VERSION%_darwin_arm64.tar" "%BINARY_NAME%" && 7z a -tgzip "../StellarCore/%BINARY_NAME%_%VERSION%_darwin_arm64.tar.gz" "../StellarCore/%BINARY_NAME%_%VERSION%_darwin_arm64.tar" && cd .. && cd ..
+del "output\StellarCore\%BINARY_NAME%" "output\StellarCore\%BINARY_NAME%_%VERSION%_darwin_arm64.tar"
 
 echo.
 echo 编译完成！
